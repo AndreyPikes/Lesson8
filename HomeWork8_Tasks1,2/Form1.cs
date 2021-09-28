54689b1e41f60d437771e44f75bc5520bfebf300
Андрей Щукин <1929999@gmail.com>
2021-09-28 21:06:46 +0300

    HomeWork8

---
+++ HomeWork8_Tasks1,2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork8_Tasks1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tbxCopyData.Text = numericUpDownData.Value.ToString();
+
+            Type typeDateTime = typeof(DateTime);
+            PropertyInfo[] propertyInfoDateTime = typeDateTime.GetProperties();
+
+            foreach (var e in propertyInfoDateTime)
+            {
+                tbxDataTime.Text += $"- {e.Name} {Environment.NewLine}";
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            tbxCopyData.Text = numericUpDownData.Value.ToString();
+        }
+    }
+}
---
+++ TrueFalseEditor/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueFalseEditor
+{
+    public class Question
+    {
+        public string Text { get; set; }
+
+        public bool TrueFalse { get; set; }
+
+        /// <summary>
+        /// конструктор по умолчанию прописан для возможности десериализации!!!
+        /// </summary>
+        public Question()
+        {
+
+        }
+
+        public Question(string text, bool trueFalse)
+        {
+            Text = text;
+            TrueFalse = trueFalse;
+        }
+    }
+}
